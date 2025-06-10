81d0759970a95c0f89716d07eec994a9ca389f3d
barran33 <barrantesj095@gmail.com>
2025-06-10 16:06:03 -0600

    seetings

--- core/settings.py
+++ core/settings.py
@@ -254,9 +254,9 @@ if not DEBUG:
 
     EMAIL_BACKEND    = os.getenv('EMAIL_BACKEND')
     EMAIL_HOST       = os.getenv('EMAIL_HOST')
-    EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 465))
-    EMAIL_USE_SSL    = os.getenv('EMAIL_USE_SSL') == True
-    EMAIL_USE_TLS    = os.getenv('EMAIL_USE_TLS') == False
+    EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 587))
+    EMAIL_USE_SSL    = os.getenv('EMAIL_USE_SSL') == False
+    EMAIL_USE_TLS    = os.getenv('EMAIL_USE_TLS') == True
     EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL')
