6d1a3346a9a1ec696e29aeea7ab75184e0974c04
barran33 <barrantesj095@gmail.com>
2025-06-10 22:21:58 -0600

    delete

--- core/settings.py
+++ core/settings.py
@@ -252,13 +252,21 @@ if not DEBUG:
 
     
 
-    EMAIL_BACKEND    = os.getenv('EMAIL_BACKEND')
-    EMAIL_HOST       = os.getenv('EMAIL_HOST')
-    EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 587))
-    EMAIL_USE_TLS    = os.getenv('EMAIL_USE_TLS') == True
-    EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL')
+    #EMAIL_BACKEND    = os.getenv('EMAIL_BACKEND')
+    #EMAIL_HOST       = os.getenv('EMAIL_HOST')
+    #EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 587))
+    #EMAIL_USE_TLS    = os.getenv('EMAIL_USE_TLS') == True
+    #EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER')
+    #EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    #DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL')
+
+    EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST='smtp.sendgrid.net'
+    EMAIL_PORT=587
+    EMAIL_USE_TLS=True
+    EMAIL_HOST_USER='apikey'
+    EMAIL_HOST_PASSWORD='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'
+    DEFAULT_FROM_EMAIL='cosmicimagination693@gmail.com'
 
     
 
