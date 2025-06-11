08ca35f7bfcf4a7d191821f6fef5c209de7457ab
barran33 <barrantesj095@gmail.com>
2025-06-10 21:05:40 -0600

    delete b&s

--- core/settings.py
+++ core/settings.py
@@ -255,7 +255,6 @@ if not DEBUG:
     EMAIL_BACKEND    = os.getenv('EMAIL_BACKEND')
     EMAIL_HOST       = os.getenv('EMAIL_HOST')
     EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 587))
-    EMAIL_USE_SSL    = os.getenv('EMAIL_USE_SSL') == False
     EMAIL_USE_TLS    = os.getenv('EMAIL_USE_TLS') == True
     EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
