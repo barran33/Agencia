841be869ad2c39c04199cc33f2636d067f5070ed
barran33 <barrantesj095@gmail.com>
2025-06-11 21:02:49 -0600

    delete

--- core/settings.py
+++ core/settings.py
@@ -261,11 +261,11 @@ if not DEBUG:
     #DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL')
 
     EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST='smtp.zoho.com'
+    EMAIL_HOST='smtp.sendgrid.net'
     EMAIL_PORT=587
     EMAIL_USE_TLS=True
+    
     DEFAULT_FROM_EMAIL='support@cosmic-imagination.com'
-
 
     
 
