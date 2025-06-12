8f75419f9318d645d027ee4a0677e45d5e9fc03a
barran33 <barrantesj095@gmail.com>
2025-06-11 22:32:09 -0600

    update

--- core/settings.py
+++ core/settings.py
@@ -256,10 +256,11 @@ if not DEBUG:
     #EMAIL_HOST       = os.getenv('EMAIL_HOST')
     #EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 587))
     #EMAIL_USE_TLS    = os.getenv('EMAIL_USE_TLS') == True
+   
+
+    #DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL')
     EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    #DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL')
-
     EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST='smtp.zoho.com'
     EMAIL_PORT=587
